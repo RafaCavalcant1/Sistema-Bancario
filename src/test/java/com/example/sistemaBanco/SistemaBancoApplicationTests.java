c7e03c12219e1aa8a0e31785cdcf1ae8d2c0830c
Rafaela cavalcanti <rafaelacavalcanti15@gmail.com>
2024-04-22 18:48:06 -0300

    feat: Project created

---
+++ src/test/java/com/example/sistemaBanco/SistemaBancoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sistemaBanco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaBancoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
