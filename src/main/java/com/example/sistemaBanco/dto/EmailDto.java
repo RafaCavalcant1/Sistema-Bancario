b378597899f2821afae47cad1bc793fd13bce745
Rafaela cavalcanti <rafaelacavalcanti15@gmail.com>
2024-05-07 10:16:04 -0300

    refactor: utilizando o records nos Dtos

--- src/main/java/com/example/sistemaBanco/dto/EmailDto.java
+++ src/main/java/com/example/sistemaBanco/dto/EmailDto.java
@@ -1,44 +1,7 @@
 package com.example.sistemaBanco.dto;
 
-public class EmailDto {
-
-	private String destinatario;
-    private String assunto;
-    private String corpo;
-    
-    public EmailDto() {
-    }
-
-	public EmailDto(String destinatario, String assunto, String corpo) {
-		super();
-		this.destinatario = destinatario;
-		this.assunto = assunto;
-		this.corpo = corpo;
-	}
-
-	public String getDestinatario() {
-		return destinatario;
-	}
-
-	public void setDestinatario(String destinatario) {
-		this.destinatario = destinatario;
-	}
-
-	public String getAssunto() {
-		return assunto;
-	}
-
-	public void setAssunto(String assunto) {
-		this.assunto = assunto;
-	}
-
-	public String getCorpo() {
-		return corpo;
-	}
-
-	public void setCorpo(String corpo) {
-		this.corpo = corpo;
-	}
-    
-    
+public record EmailDto (String destinatario,String assunto,String corpo){
+	
 }
+
+
--- src/main/java/com/example/sistemaBanco/dto/ResponseEmailDto.java
+++ src/main/java/com/example/sistemaBanco/dto/ResponseEmailDto.java
@@ -1,24 +1,5 @@
 package com.example.sistemaBanco.dto;
 
-public class ResponseEmailDto {
-
-	private String resultado;
-	
-	public ResponseEmailDto() {
-	}
-
-	public ResponseEmailDto(String resultado) {
-		super();
-		this.resultado = resultado;
-	}
-
-	public String getResultado() {
-		return resultado;
-	}
-
-	public void setResultado(String resultado) {
-		this.resultado = resultado;
-	}
-	
+public record ResponseEmailDto (String resultado){ 
 	
 }
