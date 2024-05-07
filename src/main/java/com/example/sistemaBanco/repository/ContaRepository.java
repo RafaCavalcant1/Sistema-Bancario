c0a437b1325ba494bc746850051331ae0dd24163
Rafaela cavalcanti <rafaelacavalcanti15@gmail.com>
2024-05-07 11:55:03 -0300

    feat: criação dos filtros no padrão Specification

--- src/main/java/com/example/sistemaBanco/repository/ContaRepository.java
+++ src/main/java/com/example/sistemaBanco/repository/ContaRepository.java
@@ -3,12 +3,12 @@ package com.example.sistemaBanco.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import com.example.sistemaBanco.dto.request.GetConta;
 import com.example.sistemaBanco.entities.Conta;
 
-public interface ContaRepository extends JpaRepository<Conta, Long> {
-
+public interface ContaRepository extends JpaRepository<Conta, Long>,JpaSpecificationExecutor<Conta> {
 
 	Optional<GetConta> findById(Conta contaOrigem);
 }
---
+++ src/main/java/com/example/sistemaBanco/spec/ContaSpec.java
@@ -0,0 +1,31 @@
+package com.example.sistemaBanco.spec;
+
+import org.springframework.data.jpa.domain.Specification;
+
+import com.example.sistemaBanco.entities.Conta;
+
+public class ContaSpec {
+
+	// Specification é usada para criar especificações de consulta
+	//cria uma especificação para encontrar contas com o nome de usuario parecido
+	
+	//root: representa a entidade principal da consulta, Conta
+	//query: representa a consulta em si, permite acesso a diferentes partes da consulta, como  filtros
+	//criteriaBuilder: construir as cláusulas da consulta, como where, like, equal, 
+	public static Specification<Conta> comNomeDoUsuarioParecido(String nomeCompleto) {
+		return (root, query, criteriaBuilder) -> 
+				criteriaBuilder.like(criteriaBuilder.lower(root.get("usuario").get("nomeCompleto")), "%" + nomeCompleto.toLowerCase() + "%");
+	// o like ele faz a busca de tudo a partir do % 
+				// o lower para ignorar caracteres minusculos ou maiusculos 
+	}
+	
+	public static Specification<Conta> comAgenciaParecida(String agencia) {
+		return (root, query, criteriaBuilder) -> criteriaBuilder.like(root.get("agencia"), "%" + agencia + "%");
+	}
+	
+	public static Specification<Conta> comContaIgual(String conta) {
+		return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("conta"), conta);
+	}
+	
+
+}
